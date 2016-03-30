4aaccdba6ccddbb2e1644f159f417b4439f8c367
canno <kitax2hayate5109@gmail.com>
2016-03-30 15:54:48 +0900

    ios版完成

--- app/controllers/getting_off.js
+++ app/controllers/getting_off.js
@@ -6,62 +6,67 @@ Ti.API.debug("http://gps.iwatebus.or.jp/bls/pc/" + $.args.url);
 
 // バス停の名前を取得する
 function getBusStopName() {
-	var busStopNames = [];
-	var url = "https://cryptic-journey-10666.herokuapp.com/system/getting_off.json";
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function() {
-			var json = JSON.parse(this.responseText);
-			for (var i = 0; i < json.length; i++) {
-				var names = {
-					template: "busStopTemplate",
-					properties: {
-						searchableText: json[i].bus_stop_name
-					},
-					"busStopName": {
-						text: json[i].bus_stop_name
-					},
-					"detailBtn": {
-						title: ">"
-					},
-					"url": json[i].bus_stop_url
-				};
-				busStopNames.push(names);
-			}
-			$.listSection.setItems(busStopNames);
-			$.activityIndicator.hide();
-			$.listView.setTouchEnabled(true);
-			$.search.setTouchEnabled(true);
-		},
-		onerror : function(e) {
-			Ti.API.debug(e);
-			Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
-			$.activityIndicator.hide();
-			getBusStopName();
-		},
-		timeout : 10000
-	});
-	xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
-	xhr.send();
+  var busStopNames = [];
+  var url = "https://cryptic-journey-10666.herokuapp.com/system/getting_off.json";
+  var xhr = Ti.Network.createHTTPClient({
+    onload : function() {
+      var json = JSON.parse(this.responseText);
+      for (var i = 0; i < json.length; i++) {
+        var names = {
+          template: "busStopTemplate",
+          properties: {
+            searchableText: json[i].bus_stop_name
+          },
+          "busStopName": {
+            text: json[i].bus_stop_name
+          },
+          "detailBtn": {
+            title: ">"
+          },
+          "url": json[i].bus_stop_url
+        };
+        busStopNames.push(names);
+      }
+      $.listSection.setItems(busStopNames);
+      $.activityIndicator.hide();
+      $.listView.setTouchEnabled(true);
+      $.search.setTouchEnabled(true);
+    },
+    onerror : function(e) {
+      Ti.API.debug(e);
+      if (!Ti.Network.online) {
+        Ti.UI.createAlertDialog({title: "エラー", message: e.error}).show();
+        $.activityIndicator.hide();
+        return;
+      }
+      Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
+      $.activityIndicator.hide();
+      getBusStopName();
+    },
+    timeout : 10000
+  });
+  xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
+  xhr.send();
 }
 
 function blurKeyboard(e) {
-	e.source.blur();
+  e.source.blur();
 }
 
 function openTimeTableWin(e) {
-	var item = e.section.getItemAt(e.itemIndex);
+  var item = e.section.getItemAt(e.itemIndex);
   var name = item["busStopName"]["text"];
   var url = item["url"];
-	var arg = {
-		navWin: $.args.navWin,
-		rideName: $.args.name,
-		getting_offName: name,
-		url: url
-	};
-	var timeTableWin = Alloy.createController('time_table', arg).getView();
-	$.args.navWin.openWindow(timeTableWin);
+  var arg = {
+    navWin: $.args.navWin,
+    rideName: $.args.name,
+    getting_offName: name,
+    url: url
+  };
+  var timeTableWin = Alloy.createController('time_table', arg).getView();
+  $.args.navWin.openWindow(timeTableWin);
 }
 
 function closeWin() {
-	$.gettingOffWin.close();
+  $.gettingOffWin.close();
 }
--- app/controllers/time_table.js
+++ app/controllers/time_table.js
@@ -6,157 +6,162 @@ $.activityIndicator.show();
 
 // 平日・土曜・休日の時刻表URLを取得
 function getDateUrl() {
-	var url = "https://cryptic-journey-10666.herokuapp.com/system/relevant_system.json";
-	Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function() {
-			var json = JSON.parse(this.responseText);
+  var url = "https://cryptic-journey-10666.herokuapp.com/system/relevant_system.json";
+  Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
+  var xhr = Ti.Network.createHTTPClient({
+    onload : function() {
+      var json = JSON.parse(this.responseText);
 
-			for (var i = 0; i < json.length; i++) {
-				var datesColumn = {
-					"name": json[i].name,
-					"url": json[i].url
-				};
-				dates.push(datesColumn);
-			}
-			loadWeekdayTimeTable();
-		},
-		onerror : function(e) {
-			Ti.API.debug(e);
-			Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
-			$.activityIndicator.hide();
-		},
-		timeout : 10000
-	});
-	xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
-	xhr.send();
+      for (var i = 0; i < json.length; i++) {
+        var datesColumn = {
+          "name": json[i].name,
+          "url": json[i].url
+        };
+        dates.push(datesColumn);
+      }
+      loadWeekdayTimeTable();
+    },
+    onerror : function(e) {
+      Ti.API.debug(e);
+      if (!Ti.Network.online) {
+        Ti.UI.createAlertDialog({title: "エラー", message: e.error}).show();
+        $.activityIndicator.hide();
+        return;
+      }
+      Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
+      $.activityIndicator.hide();
+    },
+    timeout : 10000
+  });
+  xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + $.args.url));
+  xhr.send();
 }
 
 function loadWeekdayTimeTable() {
-	var url = "https://cryptic-journey-10666.herokuapp.com/system/weekday.json";
-	Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[0].url));
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function() {
-			var json = JSON.parse(this.responseText);
+  var url = "https://cryptic-journey-10666.herokuapp.com/system/weekday.json";
+  Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[0].url));
+  var xhr = Ti.Network.createHTTPClient({
+    onload : function() {
+      var json = JSON.parse(this.responseText);
 
-			for (var i = 0; i < json.length; i++) {
-				var weekdayColumn = {
-					template: "timeTableTemplate",
-					"hour": {
-						text: json[i].hour
-					},
-					"times": {
-						text: json[i].times
-					}
-				};
-				weekdays.push(weekdayColumn);
-			}
-			$.listSection.setItems(weekdays);
-			loadSaturdayTimeTable();
-		},
-		onerror : function(e) {
-			Ti.API.debug(e);
-			Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
-			$.listView.setTouchEnabled(true);
-			$.activityIndicator.hide();
-		},
-		timeout : 10000
-	});
-	xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[0].url));
-	xhr.send();
+      for (var i = 0; i < json.length; i++) {
+        var weekdayColumn = {
+          template: "timeTableTemplate",
+          "hour": {
+            text: json[i].hour
+          },
+          "times": {
+            text: json[i].times
+          }
+        };
+        weekdays.push(weekdayColumn);
+      }
+      $.listSection.setItems(weekdays);
+      loadSaturdayTimeTable();
+    },
+    onerror : function(e) {
+      Ti.API.debug(e);
+      Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
+      $.listView.setTouchEnabled(true);
+      $.activityIndicator.hide();
+    },
+    timeout : 10000
+  });
+  xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[0].url));
+  xhr.send();
 }
 
 function loadSaturdayTimeTable() {
-	var url = "https://cryptic-journey-10666.herokuapp.com/system/saturday.json";
-	Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[1].url));
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function() {
-			var json = JSON.parse(this.responseText);
+  var url = "https://cryptic-journey-10666.herokuapp.com/system/saturday.json";
+  Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[1].url));
+  var xhr = Ti.Network.createHTTPClient({
+    onload : function() {
+      var json = JSON.parse(this.responseText);
 
-			for (var i = 0; i < json.length; i++) {
-				var saturdayColumn = {
-					template: "timeTableTemplate",
-					"hour": {
-						text: json[i].hour
-					},
-					"times": {
-						text: json[i].times
-					}
-				};
-				saturdays.push(saturdayColumn);
-			}
-			loadHolidayTimeTable();
-		},
-		onerror : function(e) {
-			Ti.API.debug(e);
-			Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
-			$.listView.setTouchEnabled(true);
-			$.activityIndicator.hide();
-		},
-		timeout : 10000
-	});
-	xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[1].url));
-	xhr.send();
+      for (var i = 0; i < json.length; i++) {
+        var saturdayColumn = {
+          template: "timeTableTemplate",
+          "hour": {
+            text: json[i].hour
+          },
+          "times": {
+            text: json[i].times
+          }
+        };
+        saturdays.push(saturdayColumn);
+      }
+      loadHolidayTimeTable();
+    },
+    onerror : function(e) {
+      Ti.API.debug(e);
+      Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
+      $.listView.setTouchEnabled(true);
+      $.activityIndicator.hide();
+    },
+    timeout : 10000
+  });
+  xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[1].url));
+  xhr.send();
 }
 
 function loadHolidayTimeTable() {
-	var url = "https://cryptic-journey-10666.herokuapp.com/system/holiday.json";
-	Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[2].url));
-	var xhr = Ti.Network.createHTTPClient({
-		onload : function() {
-			var json = JSON.parse(this.responseText);
+  var url = "https://cryptic-journey-10666.herokuapp.com/system/holiday.json";
+  Ti.API.debug(encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[2].url));
+  var xhr = Ti.Network.createHTTPClient({
+    onload : function() {
+      var json = JSON.parse(this.responseText);
 
-			for (var i = 0; i < json.length; i++) {
-				var holidayColumn = {
-					template: "timeTableTemplate",
-					"hour": {
-						text: json[i].hour
-					},
-					"times": {
-						text: json[i].times
-					}
-				};
-				holidays.push(holidayColumn);
-			}
-			$.listView.setTouchEnabled(true);
-			$.buttonBar.setTouchEnabled(true);
-			$.activityIndicator.hide();
-		},
-		onerror : function(e) {
-			Ti.API.debug(e);
-			Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
-			$.listView.setTouchEnabled(true);
-			$.buttonBar.setTouchEnabled(true);
-			$.activityIndicator.hide();
-		},
-		timeout : 10000
-	});
-	xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[2].url));
-	xhr.send();
+      for (var i = 0; i < json.length; i++) {
+        var holidayColumn = {
+          template: "timeTableTemplate",
+          "hour": {
+            text: json[i].hour
+          },
+          "times": {
+            text: json[i].times
+          }
+        };
+        holidays.push(holidayColumn);
+      }
+      $.listView.setTouchEnabled(true);
+      $.buttonBar.setTouchEnabled(true);
+      $.activityIndicator.hide();
+    },
+    onerror : function(e) {
+      Ti.API.debug(e);
+      Ti.UI.createAlertDialog({title: "エラー", message: "データの取得に失敗しました。"}).show();
+      $.listView.setTouchEnabled(true);
+      $.buttonBar.setTouchEnabled(true);
+      $.activityIndicator.hide();
+    },
+    timeout : 10000
+  });
+  xhr.open("GET", encodeURI(url + "?url=" + "http://gps.iwatebus.or.jp/bls/pc/" + dates[2].url));
+  xhr.send();
 }
 
 function changeTimeTable(e) {
-	switch (e.index) {
-		case 0:
-		$.timeTableTitle.setText("時刻表（平日）");
-		$.listSection.setItems(weekdays);
-		break;
-		case 1:
-		$.timeTableTitle.setText("時刻表（土曜）");
-		$.listSection.setItems(saturdays);
-		break;
-		case 2:
-		$.timeTableTitle.setText("時刻表（休日）");
-		$.listSection.setItems(holidays);
-		break;
-		default:
-		$.timeTableTitle.setText("時刻表（平日）");
-		$.listSection.setItems(weekdays);
-		break;
-	}
+  switch (e.index) {
+    case 0:
+      $.timeTableTitle.setText("時刻表（平日）");
+    $.listSection.setItems(weekdays);
+    break;
+    case 1:
+      $.timeTableTitle.setText("時刻表（土曜）");
+    $.listSection.setItems(saturdays);
+    break;
+    case 2:
+      $.timeTableTitle.setText("時刻表（休日）");
+    $.listSection.setItems(holidays);
+    break;
+    default:
+      $.timeTableTitle.setText("時刻表（平日）");
+    $.listSection.setItems(weekdays);
+    break;
+  }
 }
 
 // 画面を閉じる
 function closeWin() {
-	$.time_tableWin.close();
+  $.time_tableWin.close();
 }
