f82599fe617097cf10bd85dcdd48c1bcb24670f0
canno <kitax2hayate5109@gmail.com>
2016-04-11 18:20:59 +0900

    お気に入りの時刻表のデザインの変更

--- app/controllers/register_time_table.js
+++ app/controllers/register_time_table.js
@@ -4,7 +4,9 @@ var weekdays = [], holidays = [], saturdays = [];
 Alloy.Globals.google_analytics_trackScreen("お気に入り時刻表リスト画面");
 $.ride_getoff_Title.setText("乗車停留所：" + args.rideName + "\n" + "降車停留所：" + args.gettingOffName);
 $.activityIndicator.show();
-var date = new Date().getDay();
+var date = new Date();
+var day = date.getDay();
+var scrollItemIndex;
 
 function loadWeekdayTimeTable() {
   var url = "https://cryptic-journey-10666.herokuapp.com/system/weekday.json";
@@ -13,17 +15,30 @@ function loadWeekdayTimeTable() {
     onload : function() {
       var json = JSON.parse(this.responseText);
 
+      var trimTimesData = [];
+      for (var j = 0; j < json.length; j++) {
+        trimTimesData.push(json[j].times.trim());
+        Ti.API.debug("[" + json[j].times.trim() + "]");
+      }
+
       for (var i = 0; i < json.length; i++) {
-        var weekdayColumn = {
-          template: "timeTableTemplate",
-          "hour": {
-            text: json[i].hour
-          },
-          "times": {
-            text: json[i].times
+        var splitTimesData = trimTimesData[i].split(" ");
+        Ti.API.debug("splitTimesData: " + splitTimesData.length);
+        Ti.API.debug("[" + splitTimesData[0] + "]");
+        if (!(splitTimesData.length == 1 && splitTimesData[0] == "")) {
+          for (var k = 0; k < splitTimesData.length; k++) {
+            var weekdayColumn = {
+              template: "timeTableTemplate",
+              "hour": {
+                text: Number(json[i].hour) + ":" + splitTimesData[k],
+                color: "#333333"
+              },
+              "rate": Number(Number(json[i].hour) + splitTimesData[k])
+            };
+            Ti.API.debug("rate: " + Number(Number(json[i].hour) + splitTimesData[k]));
+            weekdays.push(weekdayColumn);
           }
-        };
-        weekdays.push(weekdayColumn);
+        }
       }
       loadSaturdayTimeTable();
     },
@@ -46,17 +61,30 @@ function loadSaturdayTimeTable() {
     onload : function() {
       var json = JSON.parse(this.responseText);
 
+      var trimTimesData = [];
+      for (var j = 0; j < json.length; j++) {
+        trimTimesData.push(json[j].times.trim());
+        Ti.API.debug("[" + json[j].times.trim() + "]");
+      }
+
       for (var i = 0; i < json.length; i++) {
-        var saturdayColumn = {
-          template: "timeTableTemplate",
-          "hour": {
-            text: json[i].hour
-          },
-          "times": {
-            text: json[i].times
+        var splitTimesData = trimTimesData[i].split(" ");
+        Ti.API.debug("splitTimesData: " + splitTimesData.length);
+        Ti.API.debug("[" + splitTimesData[0] + "]");
+        if (!(splitTimesData.length == 1 && splitTimesData[0] == "")) {
+          for (var k = 0; k < splitTimesData.length; k++) {
+            var saturdayColumn = {
+              template: "timeTableTemplate",
+              "hour": {
+                text: Number(json[i].hour) + ":" + splitTimesData[k],
+                color: "#333333"
+              },
+              "rate": Number(Number(json[i].hour) + splitTimesData[k])
+            };
+            Ti.API.debug("rate: " + Number(Number(json[i].hour) + splitTimesData[k]));
+            saturdays.push(saturdayColumn);
           }
-        };
-        saturdays.push(saturdayColumn);
+        }
       }
       loadHolidayTimeTable();
     },
@@ -79,17 +107,30 @@ function loadHolidayTimeTable() {
     onload : function() {
       var json = JSON.parse(this.responseText);
 
+      var trimTimesData = [];
+      for (var j = 0; j < json.length; j++) {
+        trimTimesData.push(json[j].times.trim());
+        Ti.API.debug("[" + json[j].times.trim() + "]");
+      }
+
       for (var i = 0; i < json.length; i++) {
-        var holidayColumn = {
-          template: "timeTableTemplate",
-          "hour": {
-            text: json[i].hour
-          },
-          "times": {
-            text: json[i].times
+        var splitTimesData = trimTimesData[i].split(" ");
+        Ti.API.debug("splitTimesData: " + splitTimesData.length);
+        Ti.API.debug("[" + splitTimesData[0] + "]");
+        if (!(splitTimesData.length == 1 && splitTimesData[0] == "")) {
+          for (var k = 0; k < splitTimesData.length; k++) {
+            var holidayColumn = {
+              template: "timeTableTemplate",
+              "hour": {
+                text: Number(json[i].hour) + ":" + splitTimesData[k],
+                color: "#333333"
+              },
+              "rate": Number(Number(json[i].hour) + splitTimesData[k])
+            };
+            Ti.API.debug("rate: " + Number(Number(json[i].hour) + splitTimesData[k]));
+            holidays.push(holidayColumn);
           }
-        };
-        holidays.push(holidayColumn);
+        }
       }
       var timetableCollection = Alloy.createCollection("timetable");
       timetableCollection.fetch({query: { statement: 'SELECT * FROM timetable WHERE rideName = ? AND gettingOffName = ?', params: [args.rideName, args.gettingOffName] }});
@@ -97,18 +138,21 @@ function loadHolidayTimeTable() {
         $.registerBtn.setBackgroundImage("/icons/star-full.png");
         $.registerBtn.saved = true;
       }
-      switch(date) {
+      switch(day) {
         case 0:
         $.timeTableTitle.setText("時刻表（休日）");
         $.listSection.setItems(holidays);
+        getScrollItemIndex(holidays);
         break;
         case 6:
         $.timeTableTitle.setText("時刻表（土曜）");
         $.listSection.setItems(saturdays);
+        getScrollItemIndex(saturdays);
         break;
         default:
         $.timeTableTitle.setText("時刻表（平日）");
         $.listSection.setItems(weekdays);
+        getScrollItemIndex(weekdays);
         break;
       }
       $.listView.setTouchEnabled(true);
@@ -129,23 +173,62 @@ function loadHolidayTimeTable() {
   xhr.send();
 }
 
+function getScrollItemIndex(timesData) {
+  var currentHour = String(date.getHours());
+  var currentMinutes;
+  if (date.getMinutes() < 10) {
+    currentMinutes = "0" + date.getMinutes();
+  } else {
+    currentMinutes = String(date.getMinutes());
+  }
+  var currentRate = Number(currentHour + currentMinutes);
+
+  Ti.API.debug("currentRate: " + currentRate);
+  Ti.API.debug("RATE: " + timesData[timesData.length - 1].rate);
+  if (currentRate > timesData[timesData.length - 1].rate) {
+    scrollItemIndex = 0;
+    changeNextDepartureTimeColor();
+    return;
+  }
+
+  for (var i = 0; i < timesData.length; i++) {
+    if (currentRate < timesData[i].rate) {
+      scrollItemIndex = i;
+      changeNextDepartureTimeColor();
+      return;
+    }
+  }
+}
+
+function changeNextDepartureTimeColor() {
+  var listItem = $.listSection.getItemAt(scrollItemIndex);
+  Ti.API.debug(listItem);
+	listItem.hour.color = "#FF0000";
+	$.listSection.updateItemAt(scrollItemIndex, listItem, {animate: true});
+  $.listView.scrollToItem(0, scrollItemIndex, {animate: true});
+}
+
 function changeTimeTable(e) {
   switch (e.index) {
     case 0:
     $.timeTableTitle.setText("時刻表（平日）");
     $.listSection.setItems(weekdays);
+    getScrollItemIndex(weekdays);
     break;
     case 1:
     $.timeTableTitle.setText("時刻表（土曜）");
     $.listSection.setItems(saturdays);
+    getScrollItemIndex(saturdays);
     break;
     case 2:
     $.timeTableTitle.setText("時刻表（休日）");
     $.listSection.setItems(holidays);
+    getScrollItemIndex(holidays);
     break;
     default:
     $.timeTableTitle.setText("時刻表（平日）");
     $.listSection.setItems(weekdays);
+    getScrollItemIndex(weekdays);
     break;
   }
 }
